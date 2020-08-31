ded932900081b16033480c387f43c075ae5c598c
Dogukan CELIK <dogukan.celik@itu.edu.tr>
2020-08-31 12:41:20 +0300

    Add unit test for BaseField() class

---
+++ model/field/test/test_base_field.py
@@ -0,0 +1,41 @@
+import unittest
+from model.field.base_field import BaseField
+
+
+class TestBaseField(unittest.TestCase):
+    def test_validate(self):
+        pass
+
+    def test_parse(self):
+        pass
+
+    def test_get_validators(self):
+        pass
+
+    def test_set_validators(self):
+        pass
+
+    def test_get_parsers(self):
+        pass
+
+    def test_set_key(self):
+        pass
+
+    def test_get_key(self):
+        pass
+
+    def test_set_value(self):
+        pass
+
+    def test_get_value(self):
+        pass
+
+    def test__validate_validators(self):
+        pass
+
+    def test__validate_parsers(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
